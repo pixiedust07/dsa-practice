8b4991b3ed75a0365c365b3e2470c1bc8e83a1c5
pixiedust07 <sharonshaji127001@gmail.com>
2022-06-20 21:45:54 +0530

    Optimize bubble sort

--- sorting/bubble.c
+++ sorting/bubble.c
@@ -1,28 +1,38 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int flag = 0, i, j;
-//try it with recursion
 
 int bubbleSort(int array[], int size)
 {
-    int temp;
-    for(i = 0; i < size; i++) {
-        for(j = 0; j < size-1; j++) {
-            if (array[j] > array[j + 1])
+    int temp, swapped;
+    do
+    {
+        swapped = false;
+        for (i = 0; i < size; i++)
+        {
+            // traverse whole array < current iteration of the outer loop (i)
+            //  since with each iteration the biggest element in the array will be bubbled up to the correct position, hence we don't need loop all the way (including the sorted elem) and just go up until before the last sorted element
+            for (j = 0; j < (size - 1 - i); j++)
             {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+                if (array[j] > array[j + 1])
+                {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                    // because if a swap occurs then we can assume that the array is not fully sorted and if the swapped remains false during an entire loop(outer loop) then it means that the array is fully sorted and no need to check remaining elements or none
+                }
+                printf("Inner-%d\n", j+1);
             }
+                printf("\nOuter-%d\n\n", i+1);
         }
-        
-    }
+    } while (swapped);
     printf("Sorted Array : ");
     for (i = 0; i < size; i++)
     {
-        printf("%d ",array[i]);
+        printf("%d ", array[i]);
     }
-    
 }
 
 void main()
@@ -41,4 +51,3 @@ void main()
 
     printf("\n");
 }
-
