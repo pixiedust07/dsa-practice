5cd76aa4cd2c2654550dafebcd3fa7696553ed8c
pixiedust07 <sharonshaji127001@gmail.com>
2022-06-22 22:41:32 +0530

    remove debug code

--- sorting/selection.c
+++ sorting/selection.c
@@ -3,33 +3,21 @@
 
 int flag = 0, i, j;
 
-int selectionSort()
+int selectionSort(int array[], int size)
 {
-
-    //two loops are correct
-    int array[10] = {1, 4, 5, 9}, size = 4;
     int currentMinimumIndex = 0, currentItemIndex = 0, temp;
-    //in the end of the loop swap currentMinimumIndex with currentItemIndex
 
     for(i = 0; i < size; i++) {
         currentMinimumIndex = i;
-        printf("\n\nCurrentMinimumIndex = %d\n", currentMinimumIndex);
-        for(j =i + 1; j < size; j++) { // ✔
-            currentItemIndex = j; // ✔
-            printf("\nCurrentItemIndex = %d \n", currentItemIndex);
-            printf("\n --Before if-- \nCurrentMinimumIndex = %d = currentItemIndex = %d", currentMinimumIndex, currentItemIndex);
+        for(j =i + 1; j < size; j++) { 
+            currentItemIndex = j;
             if(array[currentMinimumIndex] > array[j] ) {
-                currentMinimumIndex = currentItemIndex; // current lower element // ✔
-                printf("\n --inside if-- \nCurrentMinimumIndex = %d = currentItemIndex = %d", currentMinimumIndex, currentItemIndex);
+                currentMinimumIndex = currentItemIndex; 
             }
-            printf("\n --After if-- \nCurrentMinimumIndex = %d = currentItemIndex = %d", currentMinimumIndex, currentItemIndex);
-
-            printf("\n ------------inside loop end-----------\n");
         }
-        //temp = array[i];
-        //array[i] = array[currentMinimumIndex]; // ✔
-        //array[currentItemIndex] = temp;
-        printf("\n --outside loop - in [i = %d]\n", i);
+        temp = array[i];
+        array[i] = array[currentMinimumIndex]; // ✔
+        array[currentMinimumIndex] = temp;
     }
 
     printf("Sorted Array : ");
@@ -42,16 +30,16 @@ int selectionSort()
 void main()
 {
 
-    //int array[10], size, elm;
-    //printf("Enter the size of the Array :");
-    //scanf("%d", &size);
-    //printf("Enter the Array elements :");
-    //for (i = 0; i < size; i++)
-    //{
-    //    scanf("%d", &array[i]);
-    //}
+    int array[10], size, elm;
+    printf("Enter the size of the Array :");
+    scanf("%d", &size);
+    printf("Enter the Array elements :");
+    for (i = 0; i < size; i++)
+    {
+        scanf("%d", &array[i]);
+    }
 
-    selectionSort();
+    selectionSort(array, size);
 
     printf("\n");
 }
